531133b79f1580b6cfde2b2dfc018ea318a3895d
SiuKeung <siu_keung@outlook.com>
2018-07-18 20:50:24 +0800

    完成了双击编辑的功能

--- src/components/ListUnit.js
+++ src/components/ListUnit.js
@@ -8,13 +8,13 @@ export default class ListUnit extends Component {
         this.state = {
             currentFilter: '全部',
             filters: ['全部', '未完成', '已完成'],
-            items: [{id: 'b358f498-2cac-48b9-ba3a-5e264868a9', content: '做作业', checked: false, display: true},
-                {id: 'b358f498-2cac-48b9-ba3a-5e2648672a9', content: '看电视', checked: true, display: true},
-                {id: 'b358f498-2cac-48b9-ba3a-5e268678602a9', content: '打游戏', checked: false, display: true}]
+            items: [{id: 'b358f498-2cac-48b9-ba3a-5e264868a9', content: '做作业', checked: false, display: true, editable: false},
+                {id: 'b358f498-2cac-48b9-ba3a-5e2648672a9', content: '看电视', checked: true, display: true, editable: false},
+                {id: 'b358f498-2cac-48b9-ba3a-5e268678602a9', content: '打游戏', checked: false, display: true, editable: false}]
         };
         //设置监听器，监听用户是否添加新项目。
         emitter.addListener('addItemEvent', (message) => {
-            this.state.items.push({id: this.generateUUID(), content: message.trim(), checked: false, display: true});
+            this.state.items.push({id: this.generateUUID(), content: message.trim(), checked: false, display: true, editable: false});
             this.setState(this.state);
         });
     }
@@ -46,11 +46,24 @@ export default class ListUnit extends Component {
         this.setState(this.state);
     }
 
+    onSpanClicked = (id, event) => {
+        let item = this.state.items.find(current => current.id === id);
+        item.editable = true;
+        this.setState(this.state);
+    }
+
+    onSpanBlur = (id, event) => {
+        let item = this.state.items.find(current => current.id === id);
+        item.content = event.target.innerHTML;
+        this.setState(this.state);
+    }
+
     render() {
         let itemsArray = this.state.items.map(item => {
             return <li className={item.checked ? 'checked' : ''}>
-                <input name="done-todo" type="checkbox" className="done-todo" checked={item.checked} onClick={this.onCheckBoxClicked.bind(this, item.id)}/>
-                <span> {item.content}</span>
+                <input name="done-todo" type="checkbox" className="done-todo" checked={item.checked}
+                       onClick={this.onCheckBoxClicked.bind(this, item.id)}/>
+                <span contentEditable={item.editable} onClick={this.onSpanClicked.bind(this, item.id)} onBlur={this.onSpanBlur.bind(this, item.id)}> {item.content}</span>
             </li>
         });
 
