321915c715453168dd415b30e72334c6428bbbd7
SiuKeung <siu_keung@outlook.com>
2018-07-22 16:29:53 +0800

    修改了DataApi中的updateItemContent()方法，现在该方法能够向远程api发送修改列表项内容的请求； 移除了无用的方法和注释；

--- src/api/DataApi.js
+++ src/api/DataApi.js
@@ -1,44 +1,13 @@
 import filterHandlers from '../tools/FilterHandlers';
 import axios from 'axios';
 
-//用于生成列表项的id
-function generateUUID() {
-    /*jshint bitwise:false */
-    var i,
-        random;
-    var uuid = '';
-
-    for (i = 0; i < 32; i++) {
-        random = Math.random() * 16 | 0;
-        if (i === 8 || i === 12 || i === 16 || i === 20) {
-            uuid += '-';
-        }
-        uuid += (i === 12
-            ? 4
-            : (i === 16
-                ? (random & 3 | 8)
-                : random)).toString(16);
-    }
-    return uuid;
-}
-
 const DataApi = {
     items: [],
 
     allFilters: [{title: '全部', selected: true}, {title: '未完成', selected: false}, {title: '已完成', selected: false}],
 
-    getAllItems(){
-        return this.items;
-    },
-
-    getAllFilters(){
-        return this.allFilters;
-    },
 
     addItem(newItemContent, callback){
-        // let newItem = {id: generateUUID(), content: newItemContent, checked: false, display: true, editable: false};
-        // this.items.push(newItem);
-        // callback(newItem);
         axios({
             method: 'post',
             url: "http://127.0.0.1:9999/addItem",
@@ -53,37 +22,29 @@ const DataApi = {
 
     },
 
-    updateItem(id, newItem, callback){
-        let index = this.items.findIndex(item => item.id === id);
-        this.items.splice(index, 1, newItem);
-        callback();
-    },
-
     toggleCheckedStatus(id, callback){
-        let targetItem = this.items.find(item => item.id === id);
-        targetItem.checked = !targetItem.checked;
-        callback();
+        axios({
+            url: `http://127.0.0.1:9999/toggleCheckedStatus/${id}`,
+            method: 'post',
+            params: {id}
+        }).then(response => {
+            if(response.data === 'succeeded')
+                callback();
+        });
     },
 
     updateItemContent(id, newContent, callback){
-        this.items.find(item => item.id === id).content = newContent;
-        callback(id, newContent);
-    },
-
-    updateActiveFilter(title){
-        this.allFilters.forEach(item => {
-            if(item.title === title)
-                item.selected = true;
-            else
-                item.selected = false;
+        axios({
+            url: `http://127.0.0.1:9999/updateItemContent/${id}`,
+            method: 'post',
+            params: {newContent},
+        }).then(response => {
+           if(response.data === 'succeeded')
+               callback(id, newContent);
         });
     },
 
     getItemsByFilter(filterTitle, callback){
-        // let resultItems = this.items.filter(item =>
-        //     filterHandlers.find(filterItem => filterItem.title === filterTitle).handleMethod(item));
-        // callback(JSON.parse(JSON.stringify(resultItems)));
-
         axios.get(`http://127.0.0.1:9999/getItems/${filterTitle}`).then(response =>{
             let items = response.data;
             items.forEach(item => {
@@ -93,7 +54,6 @@ const DataApi = {
         });
 
     }
-
 
 
 }
