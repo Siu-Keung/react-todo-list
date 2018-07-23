c4f66b6cb41268c86eec3208dfa7d076f24a7948
SiuKeung <siu_keung@outlook.com>
2018-07-23 19:48:19 +0800

    使用ant多选框替换了原有的复选框

--- src/components/ListUnit.js
+++ src/components/ListUnit.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import '../css/ListUnit.css';
+import 'antd/dist/antd.css';
+import {Input, Checkbox} from 'antd';
 
 export default class ListUnit extends Component {
     constructor(props) {
@@ -23,8 +25,10 @@ export default class ListUnit extends Component {
             let itemElem = null;
             if (item.display) {
                 itemElem = <li className={item.checked ? 'checked' : ''}>
-                    <input name="done-todo" type="checkbox" className="done-todo" checked={item.checked}
-                           onClick={onCheckBoxClicked.bind(this, item.id)}/>
+                    {/*<input name="done-todo" type="checkbox" className="done-todo" checked={item.checked}*/}
+                           {/*onClick={onCheckBoxClicked.bind(this, item.id)}/>*/}
+                    <Checkbox checked={item.checked} onClick={onCheckBoxClicked.bind(this, item.id)}/>
+
                     {/*<span ref={this.spanContent} contentEditable={item.editable} onClick={onSpanClicked.bind(this, item.id)}*/}
                     {/*onKeyUp={this.onKeyUp.bind(this, item.id)}> {item.content}</span>*/}
                     {item.editable ?
